e1b42191272c396816a75ff848c5731846f20db7
Rahulkumar Prajapati <rpp4157@gmail.com>
2026-07-04 17:05:03 +0530

    Add GitHub Actions security pipeline

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -7,17 +7,23 @@ echo "=================================="
 # Create reports folder if it doesn't exist
 mkdir -p reports
 
-echo ""
+echo "Running Trivy Filesystem Scan..."
+trivy fs -f json -o reports/trivy-fs.json .
+
 echo "Scanning Backend Image..."
 trivy image -f json -o reports/backend.json shopnow-backend:v1
 
-echo ""
 echo "Scanning Frontend Image..."
 trivy image -f json -o reports/frontend.json shopnow-frontend:v1
 
-echo ""
 echo "Scanning Admin Image..."
 trivy image -f json -o reports/admin.json shopnow-admin:v1
+
+echo "Checking for HIGH and CRITICAL vulnerabilities..."
+trivy image \
+  --exit-code 1 \
+  --severity HIGH,CRITICAL \
+  shopnow-backend:v1
 
 echo ""
 echo "=================================="
