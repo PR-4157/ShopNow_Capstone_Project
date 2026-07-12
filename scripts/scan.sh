a5c81f381091253452a39086eb132c029460ea5c
Rahulkumar Prajapati <rpp4157@gmail.com>
2026-07-12 19:25:55 +0530

    Complete Sprint 2 automation

--- scripts/scan.sh
+++ scripts/scan.sh
@@ -4,42 +4,79 @@
 source .env
 
 echo "=================================="
-echo "Starting Trivy Image Scan"
+echo "Starting Security Scan"
 echo "=================================="
 
-# Create reports folder if it doesn't exist
-mkdir -p reports
+# Create reports directory
+mkdir -p "$REPORTS_DIR"
+
+################################################
+# GitLeaks Scan
+################################################
+
+echo ""
+echo "Running GitLeaks Scan..."
+
+gitleaks detect \
+  --source . \
+  --report-format json \
+  --report-path "$REPORTS_DIR/gitleaks-report.json"
+
+################################################
+# Trivy Filesystem Scan
+################################################
 
 echo ""
 echo "Running Trivy Filesystem Scan..."
+
 trivy fs \
   -f "$TRIVY_FORMAT" \
   -o "$REPORTS_DIR/trivy-fs.json" \
   .
 
+################################################
+# Backend Image Scan
+################################################
+
 echo ""
 echo "Scanning Backend Image..."
+
 trivy image \
   -f "$TRIVY_FORMAT" \
   -o "$REPORTS_DIR/backend-report.json" \
   "$BACKEND_IMAGE"
 
+################################################
+# Frontend Image Scan
+################################################
+
 echo ""
 echo "Scanning Frontend Image..."
+
 trivy image \
   -f "$TRIVY_FORMAT" \
-  -o "$REPORTS_DIR/backend-report.json" \
+  -o "$REPORTS_DIR/frontend-report.json" \
   "$FRONTEND_IMAGE"
+
+################################################
+# Admin Image Scan
+################################################
 
 echo ""
 echo "Scanning Admin Image..."
+
 trivy image \
   -f "$TRIVY_FORMAT" \
-  -o "$REPORTS_DIR/backend-report.json" \
+  -o "$REPORTS_DIR/admin-report.json" \
   "$ADMIN_IMAGE"
+
+################################################
+# Security Gate
+################################################
 
 echo ""
 echo "Checking for HIGH and CRITICAL vulnerabilities..."
+
 trivy image \
   --exit-code 1 \
   --severity "$SEVERITY" \
@@ -48,5 +85,5 @@ trivy image \
 echo ""
 echo "=================================="
 echo "Scanning Completed!"
-echo "Reports saved in reports/"
+echo "Reports saved in $REPORTS_DIR"
 echo "=================================="
